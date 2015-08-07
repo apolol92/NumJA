2afdda1c1c13468493e4f6ce3f81f03a30c6b9bf
apolol92 <apolol92@web.de>
2015-08-07 13:47:16 +0200

    Jacobi Iteration added
    Now we can calculate a system of linear equations..

--- src/matrix/MatrixTester.java
+++ src/matrix/MatrixTester.java
@@ -25,5 +25,22 @@ public class MatrixTester {
         System.out.print("subtraction m1-m2:\n" + m1.subtraction(m2) +"\n");
         System.out.print("addition m1+m2:\n" + m1.self_addition(m2) +"\n");
         System.out.print("subtraction m1-m2:\n" + m1.self_subtraction(m2) +"\n");
+        Matrix A = new Matrix(3,3);
+        A.values[0][0] = 3;
+        A.values[0][1] = 6;
+        A.values[0][2] = 0;
+        A.values[1][0] = 1;
+        A.values[1][1] = 7;
+        A.values[1][2] = 5;
+        A.values[2][0] = 2;
+        A.values[2][1] = 4;
+        A.values[2][2] = -8;
+        Matrix b = new Matrix(3,1);
+        b.values[0][0] = 6;
+        b.values[1][0] = 17;
+        b.values[2][0] = -12;
+        System.out.println(A);
+        System.out.println(b);
+        System.out.println(JacobiIterator.iterate(A, b,100));
     }
 }
