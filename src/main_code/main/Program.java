9149ab3c1452d2d635a02f492eaf5894ad55f987
apolol92 <apolol92@web.de>
2015-11-21 15:01:03 +0100

    repair 1

---
+++ src/main_code/main/Program.java
@@ -0,0 +1,15 @@
+package main_code.main;
+
+import main_code.matrix.MatrixTester;
+
+/**
+ * Created by apolol92 on 07.08.2015.
+ * Application class..
+ */
+public class Program {
+
+    public static void main(String args[]) {
+        MatrixTester.basic_test();
+
+    }
+}
---
+++ src/main_code/main/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Created by apolol92 on 07.08.2015.
+ * This package contains the application class called Program
+ */
+package main_code.main;
